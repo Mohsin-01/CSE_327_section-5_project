de1c02a870334a954f99f98b7e3e91f7841b7953
Mohsin Kamal <41297807+Mohsin-01@users.noreply.github.com>
2020-04-20 16:12:50 +0600

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoadAccidentApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EntryAccidentInfo()
+        {
+            return View();
+        }
+        public ActionResult ShowDriverInfo()
+        {
+            return View();
+        }
+
+        
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
